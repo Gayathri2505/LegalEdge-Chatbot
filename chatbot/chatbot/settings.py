84a69e2b36555f4eba503078d457c5e82665a7ce
Gayathri2505 <116957331+Gayathri2505@users.noreply.github.com>
2025-07-07 14:01:53 +0530

    Updated settings.py

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic'
     'users',
     'chats',
-    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
